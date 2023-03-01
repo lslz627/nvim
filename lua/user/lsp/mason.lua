782169981bf025f54a6a04c42ab492816fafd269
lslz627 <lslz627@gmail.com>
2023-03-02 02:01:38 +0800

    don't use mason auto install lsp dependence

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -20,11 +20,11 @@ local settings = {
 	max_concurrent_installers = 5,
 }
 
-require("mason").setup(settings)
-require("mason-lspconfig").setup({
-	ensure_installed = servers,
-	automatic_installation = true,
-})
+-- require("mason").setup(settings)
+-- require("mason-lspconfig").setup({
+-- 	ensure_installed = servers,
+-- 	automatic_installation = true,
+-- })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
