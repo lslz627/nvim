3e9317ff645bbabd8b0c3127b110535a78c4eb10
yong.zhang02 <yong.zhang02@mihoyo.com>
2023-12-01 23:35:02 +0800

    update treesitter version

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -82,13 +82,14 @@ return packer.startup(function(use)
 
     -- Telescope
     use { 
-        "nvim-telescope/telescope.nvim", 
+        "nvim-telescope/telescope.nvim",
         -- commit = "76ea9a898d3307244dce3573392dcf2cc38f340f" 
     }
 
     -- Treesitter
     use {
         "nvim-treesitter/nvim-treesitter",
+        tag = 'v0.9.1'
         -- commit = "8e763332b7bf7b3a426fd8707b7f5aa85823a5ac",
     }
 
